fa0d7b7364f25455acf8e02f6c913a70cc8c8f8a
Pradeep Rao Rayapudi <pradeep.rayapudi93@gmail.com>
2023-05-25 17:49:04 +0100

    user add to transport

--- transport.sql
+++ transport.sql
@@ -2,5 +2,15 @@ CREATE DATABASE transport;
 
 CREATE TABLE medium(
   medium_id SERIAL PRIMARY KEY,
-  name VARCHAR(200)
+  name VARCHAR(200),
+  type VARCHAR(200),
+  cost INTEGER
 );
+
+CREATE TABLE user(
+  user_id SERIAL PRIMARY KEY,
+  name VARCHAR(200),
+  role VARCHAR(200),
+  email VARCHAR(100),
+  password VARCHAR(50)
+)
