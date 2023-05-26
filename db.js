96f51bbea64111db976923b750218077a9a04cf6
Pradeep Rao Rayapudi <pradeep.rayapudi93@gmail.com>
2023-05-26 16:23:43 +0100

    db and other sequelize add

--- db.js
+++ db.js
@@ -1,11 +1,26 @@
-const { Pool } = require("pg");
+// Removed Code
+// const { Pool } = require("pg");
 
-const pool = new Pool({
-    user: 'pradeeprao',
-    database: 'node_app',
-    password: '',
-    port: 5432,
-    host: 'localhost',
-})
+// const pool = new Pool({
+//     user: 'pradeeprao',
+//     database: 'node_app',
+//     password: '',
+//     port: 5432,
+//     host: 'localhost',
+// })
 
-module.exports = { pool };
+// module.exports = { pool };
+const { Sequelize } = require('sequelize')
+
+const sequelize = new Sequelize('node_app', 'pradeeprao', '', {
+     host: 'localhost',
+     dialect: 'postgres'
+});
+
+sequelize.sync().then(()=>{
+   console.log('Database synced');
+}).catch((err) =>{
+  console.error('Error syncing database', err);
+});
+
+module.exports = { sequelize } 
