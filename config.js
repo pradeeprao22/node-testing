b61863c8e7c46a8d2bcfea1fb28114cb8f67e739
Pradeep Rao Rayapudi <pradeep.rayapudi93@gmail.com>
2023-05-29 15:30:10 +0100

    db seaprate file and user module define

---
+++ config.js
@@ -0,0 +1,10 @@
+module.exports = {
+    development: {
+      username: 'pradeeprao',
+      password: '',
+      database: 'node_app',
+      host: 'localhost',
+      dialect: 'postgres',
+    },
+    // Add configuration for other environments like production, test, etc.
+};
--- server.js
+++ server.js
@@ -1,17 +1,27 @@
-
-// import { sequelize } from "./db";
-const { Sequelize } = require('sequelize')
-
 // external modules
 const express = require("express")
 
+const sequelize = require("./db");
+const User = require("./models/user");
+
 const transportRoutes = require('./routes/transportRoutes')
+const userRoutes = require('./routes/userRoutes')
 
 const path = require("path")
 
 const app = express();
 
+(async () => {
+    try {
+      await sequelize.sync({ force: false });
+      console.log('Models synchronized with the database.');
+    } catch (error) {
+      console.error('Error synchronizing models:', error);
+    }
+})();
+
 app.use('/', transportRoutes)
+app.use('/users', userRoutes)
 // const port = process.env.PORT || "8000";
 
 // Middleware for parsing URL-encoded data
@@ -28,6 +38,8 @@ app.listen(3000, ()=> {
     console.log(`Node API is running on 3000`)
 })
 
+// ...
 
 
-
+  
+  // ...
